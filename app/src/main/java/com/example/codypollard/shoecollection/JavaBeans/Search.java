4d7afabd193accea08a1f9ec23a8281c7953386b
Cody Pollard <codypollard01@stclairconnect.ca>
2019-04-10 22:29:24 -0400

    Added what ever they search for into a JavaBean

---
+++ app/src/main/java/com/example/codypollard/shoecollection/JavaBeans/Search.java
@@ -0,0 +1,22 @@
+package com.example.codypollard.shoecollection.JavaBeans;
+
+public class Search {
+
+    private String search;
+
+    public Search(){
+
+    }
+
+    public Search(String search) {
+        this.search = search;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
